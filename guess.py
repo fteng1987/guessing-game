5c2dae30c9156286e5efc4df5bce7d9ecf482130
fteng1987 <fteng1987@gmail.com>
2019-07-06 18:43:19 -0400

    With five tries only.

--- guess.py
+++ guess.py
@@ -1,13 +1,20 @@
 from random import randint
 
-number = randint(1,10)
-
+number = randint(1, 10)
+count = 0
 
 while True:
-    guess = input("Please enter a number: ")
+    guess = input("Please enter a number, you have five guesses: ")
     guess = int(guess)
+
+    count += 1
     if guess == number:
         print("You've guessed correctly.")
+        break
+    
+    print("count=", count)
+    if count > 5:
+        print("You've run out of guesses.")
         break
     else:
         continue 
